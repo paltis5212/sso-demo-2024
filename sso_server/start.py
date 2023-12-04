443534fc925d39df091a71a4436d10bed15b690b
paltis5212 <paltis5212@gmail.com>
2023-12-04 16:32:08 +0800

    ✅ test: 上傳測試 sqlite

--- sso_server/start.py
+++ sso_server/start.py
@@ -5,7 +5,7 @@ app = create_app({
     'SECRET_KEY': 'secret',
     'OAUTH2_REFRESH_TOKEN_GENERATOR': True,
     'SQLALCHEMY_TRACK_MODIFICATIONS': False,
-    'SQLALCHEMY_DATABASE_URI': 'sqlite:///db.sqlite',
+    'SQLALCHEMY_DATABASE_URI': 'sqlite:///test.sqlite',
 })
 
 app.run(host="127.0.0.1", port=5001, debug=True, ssl_context="adhoc")
