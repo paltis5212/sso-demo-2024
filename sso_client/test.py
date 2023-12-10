0352b1a79a5534794c531201918909de6af03c8c
paltis5212 <paltis5212@gmail.com>
2023-12-10 22:27:40 +0800

    ✨ feature: add oauth client

--- sso_client/test.py
+++ sso_client/test.py
@@ -1,17 +1,37 @@
 from authlib.integrations.flask_client import OAuth
-oauth = OAuth()
-oauth.register(
+import certifi
+from flask import Flask, jsonify, url_for, session
+from requests import Response
+import json
+from flask_openapi3 import OpenAPI
+from sso_server.routes import get_authorize
+from authlib.integrations.flask_client import token_update
+
+from util.error_handler import set_error_handlers
+from .routes import api
+
+app = OpenAPI(__name__)
+app.config["SECRET_KEY"] = "secret"
+app.register_api(api)
+oauth = OAuth(app)
+sso = oauth.register(
     name='sso',
-    client_id='ikEN6LfzeTvi8CRgpJqGgAVZ',
-    client_secret='sBRyhpuDwaDAFlxMhXCvGVGtZbecAlOB3YZMFjNBRxYMZiKo',
-    request_token_url='https://www.svc.deltaww-energy.com:5001/oauth/token',
-    request_token_params=None,
-    access_token_url=None,
+    client_id='BHTsEyzOgQagBfSvikIytm9L',
+    client_secret='WtOq3S9TDUiynQvAQXh3UlMG8Zp8gaOKKkQ4rc4hyTzQsOKI',
+    access_token_url='https://www.svc.deltaww-energy.com:5001/oauth/token',
     access_token_params=None,
     authorize_url='https://www.svc.deltaww-energy.com:5001/oauth/authorize',
     authorize_params=None,
     api_base_url='https://www.svc.deltaww-energy.com:5001/',
-    client_kwargs=None,
+    client_kwargs={
+        'scope': 'profile',
+        'token_endpoint_auth_method': 'client_secret_basic',
+        'token_placement': 'header',
+    },
+    verify=False,
 )
+app.sso = sso
 
-client = oauth.create_client('sso')
+set_error_handlers(app)
+
+app.run(host="127.0.0.1", port=5002, debug=True, ssl_context=("ca/cert.pem", "ca/key.pem"))
