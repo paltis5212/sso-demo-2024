a6fd1ff184a44d1b0c188ab8cb0d5be6065bc697
paltis5212 <paltis5212@gmail.com>
2023-12-14 13:31:15 +0800

    ✨ feature: add service_a and _b .sqlite. ✨ feature: sso_client models.User 加上 profile 儲存、處理

--- sso_client/models.py
+++ sso_client/models.py
@@ -1,3 +1,4 @@
+import json
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -7,4 +8,13 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     source = db.Column(db.String(40), nullable=False) # 來源
     source_id = db.Column(db.Integer, nullable=False) # 來源 ID
+    profile_string = db.Column(db.String, nullable=True) # User 資料
     rand_string = db.Column(db.String, nullable=True) # 隨機字串
+
+    @property
+    def profile(self) -> dict:
+        return json.loads(self.profile_string)
+    
+    @profile.setter
+    def profile(self, value: dict):
+        self.profile_string = json.dumps(value, separators=(',', ':'))
